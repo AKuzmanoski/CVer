64e36f47e313c7090e88622a1066d86639a413cc
DS19 <dimitarspasovski93@gmail.com>
2016-03-01 18:50:33 +0100

    Adding the server side classes for retrieving a template by name

--- core/src/main/java/web/TemplatesController.java
+++ core/src/main/java/web/TemplatesController.java
@@ -5,10 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import services.TemplateService;
 
-import javax.servlet.ServletContext;
-import java.io.*;
-import java.util.Collections;
 import java.util.Map;
 
 @Controller
@@ -16,32 +14,15 @@ import java.util.Map;
 public class TemplatesController {
 
     @Autowired
-    ServletContext servletContext;
+    TemplateService templateService;
+
+    private static final String defaultTemplateName = "firstTemplate";
 
     @RequestMapping(method= RequestMethod.GET, produces = "application/json")
     @ResponseBody
     public Map getDefaultTemplate() {
 
-        File defaultTemplate = new File( servletContext.getRealPath("/WEB-INF/cv_templates/defaultTemplate.html"));
-        try {
-            BufferedReader br = new BufferedReader( new FileReader(defaultTemplate));
-            StringBuilder sb = new StringBuilder();
-            String line = null;
-            while((line = br.readLine()) != null)
-            {
-                sb.append(line);
-            }
-
-            return Collections.singletonMap("response",sb.toString());
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        return null;
+    return templateService.getTemplateByName(defaultTemplateName);
 
     }
 }
--- front/app/js/controllers/storeControllers/templateController.js
+++ front/app/js/controllers/storeControllers/templateController.js
@@ -1,14 +1,40 @@
 /**
  * Created by Dimitar on 2/25/2016.
  */
-App.controller("TemplateController",["$scope", "$state", "TemplateService", "$sce", function ($scope, $state, templateService, $sce) {
-    /**
-     * This method returns all public resumes.
-     * @returns {*|{url, method, isArray}} array of all public resumes.
-     */
-   $scope.defaultTemplate = templateService.getDefaultTemplate();
-   $scope.renderHtml = function() {
+App.controller("TemplateController",["$scope", "$state", "TemplateService", "$sce","CvService",
+    function ($scope, $state, templateService, $sce, cvService) {
+
+
+        $scope.getCv = function(account) {
+            return cvService.getCv({account: account});
+        };
+
+        //testing with an existing CV
+        $scope.cv = $scope.getCv("DimitarSpasovski");
+        console.log($scope.cv);
+
+
+        $scope.defaultTemplate = templateService.getDefaultTemplate();
+        $scope.renderHtml = function() {
        return $sce.trustAsHtml($scope.defaultTemplate.response);
-   }
+   };
+
+
+
 
 }]);
+
+
+App.directive('compileTemplate',['$compile','$parse', function($compile, $parse){
+        return {
+            link: function(scope, element, attr){
+                var parsed = $parse(attr.ngBindHtml);
+                function getStringValue() { return (parsed(scope) || '').toString(); }
+
+                //Recompile if the template changes
+                scope.$watch(getStringValue, function() {
+                    $compile(element, null, -9999)(scope);  //The -9999 makes it skip directives so that we do not recompile ourselves
+                });
+            }
+        }
+    }]);
