f669276d66de7ba32e91f6e7205aa34274b0c7a9
DS19 <dimitarspasovski93@gmail.com>
2016-07-02 12:52:10 +0200

    getting User by Email Controller

--- core/src/main/java/com/cver/team/web/PersonController.java
+++ core/src/main/java/com/cver/team/web/PersonController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/users")
@@ -14,7 +15,7 @@ public class PersonController {
     @Autowired
     PersonService personService;
 
-    @RequestMapping("/{email}")
+    @RequestMapping(value = "/{email}", method = RequestMethod.GET)
     public Person getUserByEmail(@PathVariable("email") String email) {
 
         return personService.getPersonByLoginEmailWithoutPassword(email);
